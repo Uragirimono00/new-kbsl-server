cc27c545952a1129b77468b62ff9b576f4be0115
kairos7578 <gudtj7578@gmail.com>
2023-06-02 08:56:49 +0900

    DockerFile 세팅 및 배포 테스트

---
+++ src/main/kotlin/com/example/newkbslserver/NewKbslServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.newkbslserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewKbslServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewKbslServerApplication>(*args)
+}
